f4483ffff023dcede16e6d9ae867dc5c24fc38f3
Saad <saad.tariq@gmail.com>
2020-11-08 21:23:33 +0100

    New unit tests added; alphanumeric and z

--- msc/tests/test_saad-tariq.py
+++ msc/tests/test_saad-tariq.py
@@ -3,3 +3,9 @@ from msc.rot13 import rot13_char
 
 def test_rot13_char_a():
     assert "n" == rot13_char("a"), "Unexpected character"
+
+def test_rot13_char_z():
+    assert "m" == rot13_char("z"), "Unexpected character"
+
+def test_rot13_char_alpha():
+    assert "a123" == rot13_char("a123"), "Unexpected character"
